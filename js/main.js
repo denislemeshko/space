0fe635a1def9bcfb50e76a1b0542c39502614674
DenisLemeshko <lemeshko.denis@yandex.ru>
2021-08-16 18:49:44 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-$(function(){
+$(function () {
+	$('.slider__box').slick({
+		// prevArrow:'<button type="button" class="slick-prev"></button>',
+		prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/left.svg" alt="">',
+		nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/right.svg" alt="">',
+		responsive: [
+			{
+				breakpoint: 481,
+				settings: {
+					arrows: false
+				}
+			}
+		]
+	});
 
+	$('.menu-btn').on('click', function () {
+		$('.menu__list').toggleClass('active');
+	});
 });
